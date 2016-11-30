cb13bd6cca566283bba1d7b9b28fd7ffa9477136
Colin Rundel <rundel@gmail.com>
2016-11-30 16:27:20 -0500

    Update

--- sparkr.R
+++ sparkr.R
@@ -1,5 +1,6 @@
 spark_home = "/data/spark/spark-2.0.1-bin-hadoop2.7/"
 Sys.setenv(SPARK_HOME=spark_home)
+
 library(SparkR, lib.loc = file.path(spark_home,"R/lib"))
 
 library(magrittr)
@@ -7,13 +8,18 @@ library(ggplot2)
 library(gridExtra)
 library(tidyr)
 
-sparkR.session(master="local[*]")
+sparkR.session(
+  master="local[*]", 
+  sparkConfig=list(
+    spark.executor.memory="16g", 
+    spark.driver.memory="8g"
+  )
+)
+
 
 ## JSON 
 
 reddit = read.df("/data/reddit/small.json", "json")
-
-schema(reddit)
 
 ## Subreddit
 
@@ -53,12 +59,15 @@ tmp %>%
 
 
 ### NYC Taxi 
+
+taxi_colors = c("#009E73","#F0E442")
+
 taxi_green  = read.df("/data/nyc-taxi-data/data/green_tripdata_2016-06.csv", "csv", 
                       header = "true", inferSchema = "true", na.strings = "NA")
 
 
 taxi_yellow = read.df("/data/nyc-taxi-data/data/yellow_tripdata_2016-06.csv", "csv", 
-                      header = "true", inferSchema = "true", na.strings = "NA") %>%
+                      header = "true", inferSchema = "true", na.strings = "NA") 
 
 taxi_green = taxi_green %>%
   withColumnRenamed("lpep_pickup_datetime", "pickup_datetime") %>%
@@ -98,7 +107,6 @@ taxi_pickups = rbind(
   cbind(taxi="yellow", taxi_yellow %>% clean_locs())
 )
 
-taxi_colors = c("#009E73","#F0E442")
 
 ggplot(taxi_pickups, aes(x=long, y=lat, color=taxi)) + 
   geom_point(size=0.05, alpha=0.05) + 
@@ -126,7 +134,7 @@ wday_hourly_summary = function(df)
       avg_distance = mean(tmp$Trip_distance),
       avg_time = mean(tmp$trip_time),
       avg_fare = mean(tmp$Fare_amount),
-      avg_tip = mean(tmp$Tip_amount)
+      avg_tip_perc = mean(tmp$Tip_amount / tmp$Fare_amount)
     ) %>%
     collect()
 }
@@ -140,12 +148,40 @@ taxi_wday_hourly$wday=factor(taxi_wday_hourly$wday,
                              labels = c("Sun","Mon","Tue","Wed","Thu","Fri","Sat"), 
                              ordered = TRUE)
 
-taxi_wday_hourly_tidy = gather(taxi_wday_hourly, key = avg_type, value = avg_value, avg_distance:avg_tip)
+taxi_wday_hourly_tidy = gather(taxi_wday_hourly, key = avg_type, value = avg_value, avg_distance:avg_tip_perc)
 
 
 ggplot(taxi_wday_hourly_tidy, aes(x=hour, y=avg_value, color=taxi)) +
   geom_line() + 
-  scale_colour_manual(values=colors) + 
+  scale_colour_manual(values=taxi_colors) + 
   theme_bw() +
-  facet_grid(avg_type~wday, scales="free") + 
+  facet_grid(avg_type~wday, scales="free_y") + 
   ylab("")
+
+
+
+which_wday = "Wed"
+which_hour = 16
+
+
+
+df1 = taxi_yellow %>%
+  mutate(
+    hour = hour(taxi_yellow$pickup_datetime),
+    wday = date_format(taxi_yellow$pickup_datetime, "EEE")
+  ) %>%
+  select("hour","wday","Fare_amount","Tip_amount")
+
+df2 = df1 %>%
+  filter(df1$hour == which_hour) %>%
+  filter(df1$wday == which_wday) %>%
+  mutate(tip_perc = df1$Tip_amount / df1$Fare_amount)
+  
+df2 %>% 
+  summarize(avg_tip_perc1 = mean(df2$Tip_amount / df2$Fare_amount),
+            avg_tip_perc2 = mean(df2$tip_perc)) %>%
+  collect()
+ 
+mean(collect(df2)$tip_perc,na.rm=TRUE)
+
+taxi_wday_hourly %>% dplyr::filter(wday == which_wday, hour == which_hour)
